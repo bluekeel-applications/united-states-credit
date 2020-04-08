dca047f809c8f943ea2365cdcb79eae3856e0cbc
Weston Gibler <weston@bluekeel.com>
2020-04-08 10:23:58 -0600

    Removed state from flow page and extended background color

--- src/components/FlowPage.jsx
+++ src/components/FlowPage.jsx
@@ -1,42 +1,28 @@
-import React, { useState, useEffect, useRef } from 'react';
-// import { Zoom, Fade } from 'react-reveal';
+import React from 'react';
 import FadeIn from 'react-fade-in';
 import { Link } from 'react-router-dom';
 import FlowButton from './FlowButton';
 import { FLOW_DATA } from '../utils/static';
 
 const FlowPage = ({page, handleClick}) => {
-    const componentIsMounted = useRef(true);
-    const [locals, setPageData] = useState(null);
-
-    useEffect(() => {
-        if (componentIsMounted.current) {
-            setPageData(FLOW_DATA[page]);                             
-        };
-
-        // Clean-up Function
-        return () => {componentIsMounted.current = false};
-        // eslint-disable-next-line
-    }, []);
+    const locals = FLOW_DATA[page];
 
     return (
-        locals && (
-            <FadeIn className='choose-container'>
-                <span className='flow-title-text'>{locals.titleText}</span>
-                <div className='flow-button-container'>
-                    {locals.buttons.map((button, i) => (
-                        <Link 
-                            key={`button__${button.value}__${i}`} 
-                            to={'/'+ button.value} 
-                            className='button-link' 
-                            onClick={(e) => handleClick(e, button.value, button.text)}
-                        >
-                            <FlowButton icon={button.icon} text={button.text} color={button.color} />
-                        </Link>
-                    ))}                                            
-                </div>
-            </FadeIn>
-        )
+        <FadeIn className='choose-container'>
+            <span className='flow-title-text'>{locals.titleText}</span>
+            <div className='flow-button-container'>
+                {locals.buttons.map((button, i) => (
+                    <Link 
+                        key={`button__${button.value}__${i}`} 
+                        to={'/'+ button.value} 
+                        className='button-link' 
+                        onClick={(e) => handleClick(e, button.value, button.text)}
+                    >
+                        <FlowButton icon={button.icon} text={button.text} color={button.color} />
+                    </Link>
+                ))}                                            
+            </div>
+        </FadeIn>
     );
 }
 
