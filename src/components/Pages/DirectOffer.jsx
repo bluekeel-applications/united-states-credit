cd2ec73b2da672ace71832316e3ea37aa5c93cd5
Weston Gibler <westongibler@gmail.com>
2020-08-27 07:41:12 -0600

    Promoting to staging

--- src/components/Pages/DirectOffer.jsx
+++ src/components/Pages/DirectOffer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { AppContext } from '../../context';
 import { useQuery, useMutation } from '@apollo/react-hooks';
@@ -10,46 +10,30 @@ import Loading from '../Shared/Loading';
 const DirectOffer = () => {
     let history = useHistory();
     const { trackingState, dispatchApp } = useContext(AppContext);
-    const [ readyToMove, setReady ] = useState(false);
-    
-    const handleFetchComplete = () => {
-        const offer = data.fetchDirectOffer.body;
-        dispatchApp({ type: 'SELECTED_OFFER', payload: offer });
-        addTagToServiceLog({
-            variables: {
-                service: {
-                    program_id: offer.program_id,
-                    group_id: offer.group_id,
-                    offer_id: offer.id,
-                    clickId: Number(trackingState.hsid)
-                }
-            },
-            skip: !offer.program_id || !offer.group_id || !offer.id || !trackingState.hsid
-        });
-        setReady(true);
-    };
 
     const { loading, error, data } = useQuery(DIRECT_OFFER, {
-            variables: { pid: Number(trackingState.pid),
-            onCompleted: handleFetchComplete
+            variables: { pid: Number(trackingState.pid)
         }
     });
 
     const [addTagToServiceLog] = useMutation(ADD_SERVICE_LOG);
 
     useEffect(() => {
-        if (readyToMove) {
+        if (data) {
+            const offer = data.fetchDirectOffer.body;
+            dispatchApp({ type: 'SELECTED_OFFER', payload: offer });
+            addTagToServiceLog({
+                variables: {
+                    service: {
+                        program_id: offer.program_id,
+                        group_id: offer.group_id,
+                        offer_id: offer.id,
+                        clickId: Number(trackingState.hsid)
+                    }
+                },
+                skip: !offer.program_id || !offer.group_id || !offer.id || !trackingState.hsid
+            });
             history.push('/offers');
-        };
-
-        return () => setReady(false);
-        // eslint-disable-next-line
-    }, [readyToMove]);
-
-    useEffect(() => {
-        if (data && !data.fetchDirectOffer.success) {
-            console.log('Offer not found...lets start over!', data.fetchEndpointOffer);
-            history.push('/');
             return;
         };
         // eslint-disable-next-line
