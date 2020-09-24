5e9354240de503a03f692c05bceb36fa121ced72
Weston Gibler <westongibler@gmail.com>
2020-09-24 09:30:51 -0600

    Fixed the layout of the welcome page for usc mobile

--- src/components/Pages/StyleOverrides.css.js
+++ src/components/Pages/StyleOverrides.css.js
@@ -2,7 +2,7 @@ export default {
     welcomeText: {
         fontSize: '50px',
         fontWeight: '800',
-        margin: '10vh 0 5vh 0',
+        margin: '13vh 0 5vh 0',
         color: '#092c7e',
         '@media (max-width: 767px)': {
             margin: '12vh 10px 20vh 10px',
