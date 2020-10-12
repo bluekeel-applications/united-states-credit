2c2d14d7bd57fd3f1d18d40c63156095185918a9
Weston Gibler <westongibler@gmail.com>
2020-10-12 12:12:06 -0600

    Updated the upush service worker

--- public/push-worker.js
+++ public/push-worker.js
@@ -1,1 +1,3 @@
-importScripts('https://s3.amazonaws.com/trackpush/push-worker-sdk.js');
+if(typeof window==='undefined'){try{
+    importScripts('https://pwrkr.s3.amazonaws.com/push-worker-sdk-CFVS-NE3N-1X8F-Z6ZP.js');
+}catch(e){}}
