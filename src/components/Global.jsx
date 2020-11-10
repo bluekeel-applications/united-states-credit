c620db10090845947cf0a1151c640ec9acf7f201
Weston Gibler <westongibler@gmail.com>
2020-11-10 13:43:35 -0700

    Added new sentry hook

--- src/components/Global.jsx
+++ src/components/Global.jsx
@@ -5,8 +5,9 @@ import App from './App';
 import { AppContextProvider } from '../context';
 import UseApolloClient from '@bit/bluekeel.hookz.use-apollo-client';
 import { ApolloProvider } from '@apollo/react-hooks';
-import UsePushProsScripts from '@bit/bluekeel.hookz.use-push-pros-scripts';
-import UseSentry from '@bit/bluekeel.hookz.use-sentry';
+import usePushProsScripts from '@bit/bluekeel.hookz.use-push-pros-scripts';
+import useSentry from '@bit/bluekeel.hookz.use-sentry';
+import * as Sentry from '@sentry/react';
 import Radium, { StyleRoot } from 'radium';
 // fontawesome
 import initFontAwesome from '../utils/initFontAwesome';
@@ -15,16 +16,19 @@ initFontAwesome();
 const history = createBrowserHistory();
 
 const Global = () => {
-	UsePushProsScripts('https://pushpros.tech/GetPushScript?key=2Xa3N8H4tIMDq5DaLOjgimHq4HG8UhWO&domain=');
-	UseSentry({ 
+	usePushProsScripts('https://pushpros.tech/GetPushScript?key=2Xa3N8H4tIMDq5DaLOjgimHq4HG8UhWO&domain=');
+	useSentry({
 		history,
 		dsn: 'https://e7b6d13933254ee29da1019e52d8447c@o440028.ingest.sentry.io/5407883',
 		page: 'united-states-credit',
-		version: '0.0.1'
+		version: '1.0.0'
 	});
 
-	const apolloUri = 'https://cbvb02ezl4.execute-api.us-east-1.amazonaws.com/prod/graphql';
-	// const apolloUri = 'https://uqwaw9keg3.execute-api.us-east-1.amazonaws.com/services/graphql';
+	// const apolloUri = 'https://services.bluekeel-api.com/graphql';
+	// const apolloUri = 'https://d-bkizqns6rk.execute-api.us-east-1.amazonaws.com/services/graphql';
+	// const apolloUri = 'https://cbvb02ezl4.execute-api.us-east-1.amazonaws.com/prod/graphql';
+	// const apolloUri = 'https://d234ckldiolt4m.cloudfront.net/services/graphql';
+	const apolloUri = 'https://uqwaw9keg3.execute-api.us-east-1.amazonaws.com/services/graphql';
 	// const apolloTest = 'https://cypmxq177b.execute-api.us-east-1.amazonaws.com/test/graphql';
 	// const apolloUri = 'http://localhost:4000/graphql';
 
@@ -46,4 +50,4 @@ const Global = () => {
 };
 
 
-export default Global;
+export default Sentry.withProfiler(Global, { name: 'UnitedStatesCredit', includeUpdates: false });
