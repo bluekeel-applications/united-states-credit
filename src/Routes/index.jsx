427d362fea4c510ca357c809f55efb563d42fb28
Weston Gibler <weston@bluekeel.com>
2023-03-01 12:27:17 -0400

    Added lazy loading and updated the user-end component

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,27 +1,28 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Welcome from '../components/Pages/Welcome';
 import PrivacyPolicy from '@bit/bluekeel.component-library.privacy-policy';
 import TermConditions from '@bit/bluekeel.component-library.term-conditions';
-import SelectVertical from '../components/Pages/SelectVertical';
-import SelectAutoType from '../components/Pages/SelectAutoType';
-import SelectCardType from '../components/Pages/SelectCardType';
-import SelectLoanType from '../components/Pages/SelectLoanType';
-import SelectHomeType from '../components/Pages/SelectHomeType';
-import SelectDebtType from '../components/Pages/SelectDebtType';
-import SelectDebtAmount from '../components/Pages/SelectDebtAmount';
-import SelectOptin from '../components/Pages/SelectOptin';
 import EndUserFlow from '../components/Pages/EndUserFlow';
 import UscFullLogo from '@bit/bluekeel.assets.usc_full_logo';
 import Radium from 'radium';
 import RouteWrapper from './RouteWrapper';
 
+const SelectVertical = lazy(() => import('../components/Pages/SelectVertical'));
+const SelectAutoType = lazy(() => import('../components/Pages/SelectAutoType'));
+const SelectCardType = lazy(() => import('../components/Pages/SelectCardType'));
+const SelectLoanType = lazy(() => import('../components/Pages/SelectLoanType'));
+const SelectHomeType = lazy(() => import('../components/Pages/SelectHomeType'));
+const SelectDebtType = lazy(() => import('../components/Pages/SelectDebtType'));
+const SelectDebtAmount = lazy(() => import('../components/Pages/SelectDebtAmount'));
+const SelectOptin = lazy(() => import('../components/Pages/SelectOptin'));
+
 const Policy = () => (<PrivacyPolicy domain='UnitedStatesCredit.com' logo={UscFullLogo}/>);
 const Terms = () => (<TermConditions domain='UnitedStatesCredit.com' logo={UscFullLogo}/>);
 
-const CheckingOptin = () => (<SelectOptin type='checking'/>);
-const DebtOptin = () => (<SelectOptin type='debt'/>);
+// const CheckingOptin = () => (<SelectOptin type='checking'/>);
+// const DebtOptin = () => (<SelectOptin type='debt'/>);
 
 const Routes = () => (
     <RouteWrapper>
@@ -29,15 +30,51 @@ const Routes = () => (
             <Route path='/' exact component={Welcome} />
             <Route path='/privacy' component={Policy} />
             <Route path='/terms' component={Terms} />
-            <Route path='/verticals' component={SelectVertical} />
-            <Route path='/credit_cards' component={SelectCardType} />
-            <Route path='/auto_loans' component={SelectAutoType} />
-            <Route path='/home_loans' component={SelectHomeType} />
-            <Route path='/personal_loans' component={SelectLoanType} />
-            <Route path='/debt_types' component={SelectDebtType} />
-            <Route path='/debt_amount' component={SelectDebtAmount} />
-            <Route path='/debt_optin' component={DebtOptin} />
-            <Route path='/checking' component={CheckingOptin} />
+            <Route path='/verticals' element={
+                <Suspense fallback={<>...</>}>
+                    <SelectVertical/>
+                </Suspense>
+            } />
+            <Route path='/credit_cards' element={
+                <Suspense fallback={<>...</>}>
+                    <SelectCardType/>
+                </Suspense>
+            } />
+            <Route path='/auto_loans' element={
+                <Suspense fallback={<>...</>}>
+                    <SelectAutoType/>
+                </Suspense>
+            } />
+            <Route path='/home_loans' element={
+                <Suspense fallback={<>...</>}>
+                    <SelectHomeType/>
+                </Suspense>
+            } />
+            <Route path='/personal_loans' element={
+                <Suspense fallback={<>...</>}>
+                    <SelectLoanType/>
+                </Suspense>
+            } />
+            <Route path='/debt_types' element={
+                <Suspense fallback={<>...</>}>
+                    <SelectDebtType/>
+                </Suspense>
+            } />
+            <Route path='/debt_amount' element={
+                <Suspense fallback={<>...</>}>
+                    <SelectDebtAmount/>
+                </Suspense>
+            } />
+            <Route path='/debt_optin' element={
+                <Suspense fallback={<>...</>}>
+                    <SelectOptin type='debt'/>
+                </Suspense>
+            } />
+            <Route path='/checking' element={
+                <Suspense fallback={<>...</>}>
+                    <SelectOptin type='checking'/>
+                </Suspense>
+            } />
             <Route path='/email_optin' component={EndUserFlow} />
         </Switch>
     </RouteWrapper>
