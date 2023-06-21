bbe4302f86956486a16c406dfc15da999f69e3f6
Weston Gibler <weston@bluekeel.com>
2023-06-21 12:17:07 -0400

    Finished the metals article. I also fixed LazyLoading

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,16 +1,17 @@
 import React, { lazy, Suspense } from 'react';
-import { Route, Switch } from 'react-router-dom';
-
-import Welcome from '../components/Pages/Welcome';
-import PrivacyPolicy from '@bit/bluekeel.component-library.privacy-policy';
-import TermConditions from '@bit/bluekeel.component-library.term-conditions';
-import EndUserFlow from '../components/Pages/EndUserFlow';
+import { BrowserRouter as Switch, Route } from 'react-router-dom';
 import UscFullLogo from '@bit/bluekeel.assets.usc_full_logo';
 import Radium from 'radium';
-import RouteWrapper from './RouteWrapper';
-import OptInESP from '../components/Pages/OptInESP';
-import DuplicateCheck from '../components/Pages/DuplicateCheck';
-import System1 from '../components/Pages/System1';
+import Loading from '../components/Shared/Loading';
+
+const Welcome = lazy(() => import('../components/Pages/Welcome'));
+const PrivacyPolicy = lazy(() => import('@bit/bluekeel.component-library.privacy-policy'));
+const TermConditions = lazy(() => import('@bit/bluekeel.component-library.term-conditions'));
+const EndUserFlow = lazy(() => import('../components/Pages/EndUserFlow'));
+const RouteWrapper = lazy(() => import('./RouteWrapper'));
+const OptInESP = lazy(() => import('../components/Pages/OptInESP'));
+const DuplicateCheck = lazy(() => import('../components/Pages/DuplicateCheck'));
+const System1 = lazy(() => import('../components/Pages/System1'));
 
 const SelectVertical = lazy(() => import('../components/Pages/SelectVertical'));
 const SelectAutoType = lazy(() => import('../components/Pages/SelectAutoType'));
@@ -21,69 +22,30 @@ const SelectDebtType = lazy(() => import('../components/Pages/SelectDebtType'));
 const SelectDebtAmount = lazy(() => import('../components/Pages/SelectDebtAmount'));
 const SelectOptin = lazy(() => import('../components/Pages/SelectOptin'));
 
-const Policy = () => (<PrivacyPolicy domain='UnitedStatesCredit.com' logo={UscFullLogo}/>);
-const Terms = () => (<TermConditions domain='UnitedStatesCredit.com' logo={UscFullLogo}/>);
-
-// const CheckingOptin = () => (<SelectOptin type='checking'/>);
-// const DebtOptin = () => (<SelectOptin type='debt'/>);
-
 const Routes = () => (
-    <RouteWrapper>
-        <Switch>
-            <Route path='/' exact component={Welcome} />
-            <Route path='/privacy' component={Policy} />
-            <Route path='/terms' component={Terms} />
-            <Route path='/join' component={OptInESP} />
-            <Route path='/verticals' element={
-                <Suspense fallback={<>...</>}>
-                    <SelectVertical/>
-                </Suspense>
-            } />
-            <Route path='/credit_cards' element={
-                <Suspense fallback={<>...</>}>
-                    <SelectCardType/>
-                </Suspense>
-            } />
-            <Route path='/auto_loans' element={
-                <Suspense fallback={<>...</>}>
-                    <SelectAutoType/>
-                </Suspense>
-            } />
-            <Route path='/home_loans' element={
-                <Suspense fallback={<>...</>}>
-                    <SelectHomeType/>
-                </Suspense>
-            } />
-            <Route path='/personal_loans' element={
-                <Suspense fallback={<>...</>}>
-                    <SelectLoanType/>
-                </Suspense>
-            } />
-            <Route path='/debt_types' element={
-                <Suspense fallback={<>...</>}>
-                    <SelectDebtType/>
-                </Suspense>
-            } />
-            <Route path='/debt_amount' element={
-                <Suspense fallback={<>...</>}>
-                    <SelectDebtAmount/>
-                </Suspense>
-            } />
-            <Route path='/debt_optin' element={
-                <Suspense fallback={<>...</>}>
-                    <SelectOptin type='debt'/>
-                </Suspense>
-            } />
-            <Route path='/checking' element={
-                <Suspense fallback={<>...</>}>
-                    <SelectOptin type='checking'/>
-                </Suspense>
-            } />
-            <Route path='/email_optin' component={EndUserFlow} />
-            <Route path='/duplicate_check' component={DuplicateCheck} />
-            <Route path='/rsoc' component={System1} />
-        </Switch>
-    </RouteWrapper>
+
+        <Suspense fallback={<Loading />}>
+            <RouteWrapper>
+                <Switch>
+                    <Route path='/' exact ><Welcome /></Route>
+                    <Route path='/privacy'><PrivacyPolicy domain='UnitedStatesCredit.com' logo={UscFullLogo}/></Route>
+                    <Route path='/terms'><TermConditions domain='UnitedStatesCredit.com' logo={UscFullLogo}/></Route>
+                    <Route path='/join'><OptInESP /></Route>
+                    <Route path='/verticals'><SelectVertical/></Route>
+                    <Route path='/credit_cards'><SelectCardType/></Route>
+                    <Route path='/auto_loans'><SelectAutoType/></Route>
+                    <Route path='/home_loans'><SelectHomeType/></Route>
+                    <Route path='/personal_loans'><SelectLoanType/></Route>
+                    <Route path='/debt_types'><SelectDebtType/></Route>
+                    <Route path='/debt_amount'><SelectDebtAmount/></Route>
+                    <Route path='/debt_optin'><SelectOptin type='debt'/></Route>
+                    <Route path='/checking'><SelectOptin type='checking'/></Route>
+                    <Route path='/email_optin'><EndUserFlow /></Route>
+                    <Route path='/duplicate_check'><DuplicateCheck /></Route>
+                    <Route path='/rsoc'><System1 /></Route>
+                </Switch>
+            </RouteWrapper>
+        </Suspense>
 );
 
 export default Radium(Routes);
--- src/components/Pages/System1/Articles/MetalsArticle.jsx
+++ src/components/Pages/System1/Articles/MetalsArticle.jsx
@@ -16,39 +16,40 @@ const MetalsArticle = () => {
         <div style={styles.mainConatiner}>
             <div style={styles.contentContainer}>
                 <div>
-                Tax issues can be daunting, especially when you find yourself in a situation where you owe a significant amount or are facing an audit. In such cases, tax resolution companies can provide invaluable assistance. These specialized firms are equipped with the knowledge and expertise to help individuals and businesses navigate through complex tax matters and find viable solutions. In this article, we will delve into the world of tax resolution companies, exploring their role, services, and how they can potentially alleviate your tax-related burdens.
+                Throughout history, precious metals have held a special place as a store of value and a hedge against economic uncertainty. In today's ever-changing financial landscape, owning precious metals can offer several benefits to investors and individuals seeking to diversify their portfolios. This article delves into the importance of buying and owning precious metals, highlighting their historical significance, value as a tangible asset, and potential for long-term wealth preservation.
                 </div>
             </div>
             <div style={styles.contentContainer}>
-                <ArticleTitle text="Understanding Tax Resolution Companies:"/>
-                <ContentText text='Tax resolution companies are professional firms that specialize in resolving tax problems on behalf of their clients. They employ tax professionals, including enrolled agents, certified public accountants (CPAs), and tax attorneys, who possess in-depth knowledge of tax laws and regulations. These professionals work closely with clients to identify and address their specific tax concerns.' />
+                <ArticleTitle text="I. Historical Significance:"/>
+                <ContentText text='Precious metals like gold, silver, platinum, and palladium have been prized for centuries for their rarity, beauty, and inherent value.' />
+                <ContentText text='These metals have served as mediums of exchange, a store of wealth, and a unit of account in various civilizations, transcending time and cultural boundaries.' />
+                <ContentText text='The enduring demand for precious metals underscores their status as reliable assets.' />
             </div>
             <div style={styles.contentContainer}>
-                <ArticleTitle text='Services Offered by Tax Resolution Companies:'/>
-                <ContentText title='1. Tax Negotiation and Settlement' text='Tax resolution companies can negotiate with the Internal Revenue Service (IRS) or state tax authorities on your behalf. They explore options such as offers in compromise, installment agreements, or penalty abatement to help reduce the overall tax liability or establish manageable payment plans.' />
-                <ContentText title='2. Audit Representation' text='If you face an IRS audit, tax resolution companies can provide representation and guide you through the audit process. They ensure that your rights are protected, help gather necessary documentation, and present your case effectively.' />
-                <ContentText title='3. Tax Debt Resolution' text='If you have accumulated tax debt, tax resolution companies can develop strategies to address it. They analyze your financial situation, assess eligibility for debt reduction programs, and negotiate with tax authorities to find a feasible resolution.' />
-                <ContentText title='4. Penalty and Interest Abatement' text='In certain cases, tax resolution professionals can help reduce or eliminate penalties and interest associated with tax debts. They review your circumstances and work towards reaching a favorable outcome.' />
-                <ContentText title='5. Offer in Compromise (OIC) Assistance' text='Tax resolution companies can guide you through the process of an offer in compromise. This option allows qualifying taxpayers to settle their tax debt for less than the full amount owed. Skilled professionals help determine eligibility, gather required documentation, and present a compelling case to the IRS.' />
-            </div>
-            {/* <ButtonContainer containerId='rampjs_slot2' email={trackingState['email']} title='Search and Compare Precious Metals'/> */}
-            <div style={styles.contentContainer}>
-                <ArticleTitle text='Benefits of Engaging a Tax Resolution Company:'/>
-                <ContentText title='1. Expertise and Experience' text="Tax resolution companies have a deep understanding of tax laws, regulations, and negotiation techniques. Their professionals are well-versed in handling complex tax matters, ensuring that you receive the most accurate advice and effective representation." />
-                <ContentText title='2. Time and Stress Savings' text="Dealing with tax issues can be time-consuming and stressful. Engaging a tax resolution company allows you to offload the burden to experienced professionals who can handle the complexities on your behalf. This frees up your time to focus on other important aspects of your life or business." />
-                <ContentText title='3. Improved Communication' text="Tax resolution professionals act as intermediaries between you and tax authorities. They communicate with tax agencies on your behalf, ensuring that your rights are protected and minimizing the chances of miscommunication or misunderstandings." />
+                <ArticleTitle text="II. Tangible Asset Value:"/>
+                <ContentText text='Precious metals are physical assets that can be held directly, providing a tangible form of wealth.' />
+                <ContentText text='Unlike stocks, bonds, or digital currencies, which are subject to market volatility and potential cyber threats, precious metals offer a level of security and privacy.' />
             </div>
             <div style={styles.contentContainer}>
-                <ArticleTitle text='Choosing the Right Tax Resolution Company:'/>
+                <ArticleTitle text="III. Hedge Against Economic Uncertainty:"/>
+                <ContentText text='Precious metals have historically demonstrated resilience during times of economic instability, currency devaluation, and inflationary pressures.' />
+                <ContentText text='They act as a hedge against inflation, preserving purchasing power when traditional fiat currencies lose value.' />
+                <ContentText text='During market downturns or financial crises, the value of precious metals tends to rise, providing a potential safe haven for investors.' />
             </div>
             <div style={styles.contentContainer}>
-                <ArticleTitle text='When selecting a tax resolution company, consider the following factors:'/>
-                <ContentText title='Reputation and Credentials' text="Research the company's reputation, including client reviews and professional accreditations." />
-                <ContentText title='Transparent Pricing' text="Ensure that the company provides clear and transparent pricing information upfront." />
-                <ContentText title='Personalized Approach' text="Look for a company that offers tailored solutions based on your specific tax situation." />
+                <ArticleTitle text="IV. Portfolio Diversification:"/>
+                <ContentText text='Including precious metals in an investment portfolio can enhance diversification, reducing overall risk.' />
+                <ContentText text='Precious metals typically have a low correlation with other asset classes such as stocks and bonds, which means they may perform differently under varying market conditions.' />
+                <ContentText text='A diversified portfolio that includes precious metals can help offset losses during market downturns and potentially enhance long-term returns.' />
             </div>
             <div style={styles.contentContainer}>
-                <ContentText text="Tax resolution companies play a vital role in helping individuals and businesses navigate through complex tax challenges. Their expertise, experience, and understanding of tax laws can alleviate the burden of dealing with tax problems and provide you with peace of mind. If you find yourself in a tax predicament, consider consulting a reputable tax resolution company to explore viable solutions and work towards resolving your tax issues efficiently and effectively." />
+                <ArticleTitle text="V. Wealth Preservation:"/>
+                <ContentText text='Precious metals have demonstrated their ability to preserve wealth over time.' />
+                <ContentText text='Historical data shows that gold and other precious metals have maintained their purchasing power and even outperformed other assets during certain periods.' />
+                <ContentText text='The scarcity and global demand for these metals contribute to their long-term value.' />
+            </div>
+            <div style={styles.contentContainer}>
+                <ContentText text='The importance of buying and owning precious metals goes beyond their intrinsic beauty and historical significance. They offer a tangible form of wealth, act as a hedge against economic uncertainties, and provide portfolio diversification. While market fluctuations and short-term volatility may impact the price of precious metals, their long-term value has proven to be resilient. Including precious metals in your investment strategy can contribute to wealth preservation and offer peace of mind in times of economic instability. As always, it is advisable to consult with a financial advisor to determine the best approach based on your individual circumstances and investment goals.' />
             </div>
         </div>
     );
