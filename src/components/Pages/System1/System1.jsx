c7c91678865bce2ea67b30aff728dbc5ae1119f2
Weston Gibler <master@westons-mbp.mynetworksettings.com>
2023-09-05 10:34:06 -0600

    Fixed the sub header language to be if interested

--- src/components/Pages/System1/System1.jsx
+++ src/components/Pages/System1/System1.jsx
@@ -33,79 +33,79 @@ const System1 = () => {
         switch(trackingState.article) {
             case 'credit':
                 setHeaderText('The Best Credit Cards in 2023 for Any Credit Type');
-                setHeaderSubText('Finding the perfect credit card can extend our spending abilities and help us manage our personal finances.  Use the buttons below to search for and compare the best credit card options currently available.');
+                setHeaderSubText('Finding the perfect credit card can extend our spending abilities and help us manage our personal finances.  If interested, you can search for and compare the best credit card options currently available.');
                 setButtonQuestion('Search and Compare Credit Cards');
                 setArticle(<CreditCardArticle />);
                 break;
             case 'loan':
                 setHeaderText('Find the Best Loans in 2023 from $500-$20,000');
-                setHeaderSubText('Finding the right personal loan can save you money, provide you with financial flexibility and improve your credit score.   Use the buttons below to search for and compare the best personal loan options.');
+                setHeaderSubText('Finding the right personal loan can save you money, provide you with financial flexibility and improve your credit score.   If interested, you can search for and compare the best personal loan options.');
                 setButtonQuestion('Search and Compare Personal Loans');
                 setArticle(<LoanArticle />);
                 break;
             case 'saving':
                 setHeaderText('Earn $1,000s in Passive Income with a High Yield Savings Account');
-                setHeaderSubText('A high yield savings account is a safe and effective way to maximize your earnings on interest.  Use the buttons below to search for and compare banks with the highest yield on savings, and start adding $1,000s per year in passive income.');
+                setHeaderSubText('A high yield savings account is a safe and effective way to maximize your earnings on interest.  If interested, you can search for and compare banks with the highest yield on savings, and start adding $1,000s per year in passive income.');
                 setButtonQuestion('Search and Compare High Yield Savings');
                 setArticle(<SavingsArticle />);
                 break;
             case 'tax':
                 setHeaderText('Eliminate $1,000s in Tax Debt and Stop Harassment from the IRS');
-                setHeaderSubText('Tax resolution offers just about anyone the ability to lower, if not eliminate, past tax debt.  Use the buttons below to search for and compare the top tax resolution companies and hit the reset button on past taxes.  Most credible services provide a 100% money back guarantee.');
+                setHeaderSubText('Tax resolution offers just about anyone the ability to lower, if not eliminate, past tax debt.  If interested, you can search for and compare the top tax resolution companies and hit the reset button on past taxes.  Most credible services provide a 100% money back guarantee.');
                 setButtonQuestion('Search and Compare Tax Resolution');
                 setArticle(<TaxArticle />);
                 break;
             case 'metal':
                 setHeaderText('Safeguard and Preserve Your Wealth By Investing In Precious Metals');
-                setHeaderSubText('Buying precious metals can preserve wealth, act as a hedge against inflation, and offer diversification benefits for investment portfolios.   Use the buttons below to search for and compare the best deals on purchasing gold, silver and other precious metals.');
+                setHeaderSubText('Buying precious metals can preserve wealth, act as a hedge against inflation, and offer diversification benefits for investment portfolios.   If interested, you can search for and compare the best deals on purchasing gold, silver and other precious metals.');
                 setButtonQuestion('Search and Compare Precious Metals');
                 setArticle(<MetalsArticle />);
                 break;
             case 'life':
                 setHeaderText('Find The Best, Low Cost Life Insurance Policy');
-                setHeaderSubText('Life Insurance is an inexpensive, yet essential financial tool that provides protection and peace of mind for individuals and their loved ones.  Use the buttons below to search for and compare the top life insurance companies and secure peace of mind and a lasting legacy for those you care about.');
+                setHeaderSubText('Life Insurance is an inexpensive, yet essential financial tool that provides protection and peace of mind for individuals and their loved ones.  If interested, you can search for and compare the top life insurance companies and secure peace of mind and a lasting legacy for those you care about.');
                 setButtonQuestion('Search and Compare Life Insurance');
                 setArticle(<LifeInsuranceArticle />);
                 break;
             case 'home':
                 setHeaderText('How to Find the Perfect Home Warranty on Any Budget');
-                setHeaderSubText('A home warranty is an invaluable, cost effective way to protect yourself from unexpected expenses. Use the buttons below to search for and compare top home warranty offers and gain an important peace of mind.');
+                setHeaderSubText('A home warranty is an invaluable, cost effective way to protect yourself from unexpected expenses. If interested, you can search for and compare top home warranty offers and gain an important peace of mind.');
                 setButtonQuestion('Search and Compare Home Warranties');
                 setArticle(<HomeWarrantyArticle />);
                 break;
             case 'security':
                 setHeaderText('Find The Best Home Security Systems of 2023');
-                setHeaderSubText('A home security system is an essential, yet cost effective tool for protecting our loved ones and our valuable belongings. Use the buttons below to search for and compare top home security systems and gain an important peace of mind.');
+                setHeaderSubText('A home security system is an essential, yet cost effective tool for protecting our loved ones and our valuable belongings. If interested, you can search for and compare top home security systems and gain an important peace of mind.');
                 setButtonQuestion('Search and Compare Home Security');
                 setArticle(<HomeSecurityArticle />);
                 break;
             case 'edu':
                 setHeaderText('Double Your Income With an Online Degree');
-                setHeaderSubText('Those with college degrees earn an additional $1.2 million dollars in their lifetime. It’s a cost effective alternative to a 4 year University and takes only a few hours a week. Use the buttons below to search for and compare top online universities and degree programs.');
+                setHeaderSubText('Those with college degrees earn an additional $1.2 million dollars in their lifetime. It’s a cost effective alternative to a 4 year University and takes only a few hours a week. If interested, you can search for and compare top online universities and degree programs.');
                 setButtonQuestion('Search and Compare Online Degrees');
                 setArticle(<EducationArticle />);
                 break;
             case 'debt':
                 setHeaderText('How a Debt Consolidation Company Can Help Eliminate Your Debt');
-                setHeaderSubText('A reputable debt consolidation company can simplify your payments and negotiate reduced payment amounts with your creditors.  Use the buttons below to search for and compare top debt consolidation companies.');
+                setHeaderSubText('A reputable debt consolidation company can simplify your payments and negotiate reduced payment amounts with your creditors.  If interested, you can search for and compare top debt consolidation companies.');
                 setButtonQuestion('Search and Compare Debt Consolidation');
                 setArticle(<DebtArticle />);
                 break;
             case 'injury':
                 setHeaderText('Get Every Penny You Are Owed – How to Find the Best Personal Injury Attorney');
-                setHeaderSubText('Accidents can happen unexpectedly, leaving victims with physical, emotional, and financial burdens. In such distressing times, a personal injury attorney can be your lifeline and ensure you receive fair compensation for your losses.  Use the buttons below to search for and compare the best personal injury attornies.');
-                setButtonQuestion('Search and Compare Personal Injury Attornies');
+                setHeaderSubText('Accidents can happen unexpectedly, leaving victims with physical, emotional, and financial burdens. In such distressing times, a personal injury attorney can be your lifeline and ensure you receive fair compensation for your losses.  If interested, you can search for and compare the best personal injury attornies.');
+                setButtonQuestion('Search and Compare Personal Injury Attorneys');
                 setArticle(<PersonalInjuryArticle />);
                 break;
             case 'auto':
                 setHeaderText('The Importance of Extended Auto Warranties');
-                setHeaderSubText('As the cost of owning a vehicle continues to rise, one essential tool in protecting yourself against unexpected repairs and maintenance expenses is an extended auto warranty. Use the buttons below to search for and compare the best extended auto warranties.');
+                setHeaderSubText('As the cost of owning a vehicle continues to rise, one essential tool in protecting yourself against unexpected repairs and maintenance expenses is an extended auto warranty. If interested, you can search for and compare the best extended auto warranties.');
                 setButtonQuestion('Search and Compare Extended Auto Warranties');
                 setArticle(<AutoWarrantyArticle />);
                 break;
             case 'score':
                 setHeaderText('The Crucial Role of Your Credit Score: Why Monitoring It Matters');
-                setHeaderSubText("In today's financial landscape, your credit score wields an unprecedented influence over your life. As such, monitoring your credit score has become not just a good practice, but a necessary one. Use the buttons below to search for and compare the best free credit score and monitoring services.");
+                setHeaderSubText("In today's financial landscape, your credit score wields an unprecedented influence over your life. As such, monitoring your credit score has become not just a good practice, but a necessary one. If interested, you can search for and compare the best free credit score and monitoring services.");
                 setButtonQuestion('Search and Compare Credit Score Monitors');
                 setArticle(<CreditScoreArticle />);
                 break;
