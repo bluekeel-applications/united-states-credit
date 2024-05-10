287abda40c3ad46df56e12c91e98fb4560a6194a
Weston Gibler <master@Westons-MacBook-Pro.local>
2024-05-10 13:15:05 -0400

    Added rec offer to life insurance article

--- src/components/Pages/System1/Articles/LifeInsuranceArticle.jsx
+++ src/components/Pages/System1/Articles/LifeInsuranceArticle.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-// import { AppContext } from '../../../../context';
+import React, { useContext } from 'react';
+import { AppContext } from '../../../../context';
 import styles from './Articles.css.js';
-// import ButtonContainer from '../ButtonContainer';
 import ContentText from './components/ContentText';
-// import RecommendBox from './components/RecommendBox.jsx';
+import RecommendBox from './components/RecommendBox.jsx';
 
 const LifeInsuranceArticle = () => {
-    // const { trackingState } = useContext(AppContext);
+    const { trackingState } = useContext(AppContext);
 
     return(
         <div style={styles.mainConatiner}>
@@ -15,13 +14,13 @@ const LifeInsuranceArticle = () => {
                 Life insurance is an essential financial tool that provides protection and peace of mind for individuals and their loved ones. It serves as a safety net, ensuring that financial responsibilities are met and loved ones are taken care of in the event of the policyholder's death. However, finding the right life insurance policy can be a complex and overwhelming process. In this article, we will discuss the importance of life insurance and provide guidance on how to find the policy that best suits your needs.
                 </div>
             </div>
-            {/* <RecommendBox
-                mainTitle="Editor's Choice: Access Today’s Top Loan Offer for Any Credit Type"
+            <RecommendBox
+                mainTitle="Editor's Choice: Access Today’s Top Life Insurance Offer"
                 titleText="Why we like it"
-                text="This offer caters to individuals with less-than-perfect credit, providing quick access to personal loans or emergency funds. The application process is speedy, uncomplicated, and direct."
-                offerUrl={`https://www.bkoffers.com/hitstreet/redirect_tp.cfm?oid=19&sid=9478&pid=3361&eid=top-${trackingState.sid}&uid=yourUID`}
+                text="This offer caters to individuals looking to protect their families future and offers a variety of options based on your financial needs. The application process is speedy, uncomplicated, and direct."
+                offerUrl={`https://www.bkoffers.com/hitstreet/redirect_tp.cfm?oid=19&sid=9840&pid=3577&eid=top-${trackingState.sid}&uid=yourUID`}
                 cta="ACCESS NOW"
-            /> */}
+            />
             <div style={styles.contentContainer}>
                 <ContentText text='The importance of life insurance cannot be overstated, especially for those who have dependents or financial obligations. Here are some key reasons why having life insurance is crucial:' />
                 <ContentText title='1. Financial Security for Loved Ones' text="Life insurance offers financial protection to your loved ones in the event of your untimely demise. It provides a tax-free lump sum payment, known as the death benefit, to your beneficiaries. This money can be used to replace lost income, pay off debts, cover funeral expenses, or fund your children's education." />
@@ -29,7 +28,6 @@ const LifeInsuranceArticle = () => {
                 <ContentText title='3. Income Replacement' text='If you are the primary breadwinner in your family, life insurance can provide a source of income replacement. It can help your dependents maintain their current standard of living by replacing the income that would have been generated had you been alive.' />
                 <ContentText title='4. Business Continuity' text='Life insurance is also crucial for business owners. It can help protect the continuity of a business by providing funds for buy-sell agreements, key person insurance, or covering outstanding business debts.' />
             </div>
-            {/* <ButtonContainer containerId='rampjs_slot2' email={trackingState['email']} title='Search and Compare Tax Resolution'/> */}
             <div style={styles.contentContainer}>
                 <ContentText text="Now that we understand the importance of life insurance, let's explore how to find the right policy:" />
                 <ContentText title='1. Assess Your Needs' text="Begin by assessing your financial situation, current obligations, and future goals. Consider factors such as your age, income, dependents, outstanding debts, and long-term financial plans. This evaluation will help you determine the amount of coverage you need." />
@@ -44,13 +42,13 @@ const LifeInsuranceArticle = () => {
             <div style={styles.contentContainer}>
                 <ContentText text="In conclusion, life insurance plays a vital role in securing the financial future of your loved ones. By understanding the importance of life insurance and following these steps to find the right policy, you can ensure that your family is protected financially, even in the face of unexpected events. Take the time to assess your needs, explore the options available, and seek professional advice to make an informed decision. Remember, life insurance provides peace of mind and a lasting legacy for those you care about." />
             </div>
-            {/* <RecommendBox
-                mainTitle="Editor's Choice: Access Today’s Top Loan Offer for Any Credit Type"
+            <RecommendBox
+                mainTitle="Editor's Choice: Access Today’s Top Life Insurance Offer"
                 titleText="Why we like it"
-                text="This offer caters to individuals with less-than-perfect credit, providing quick access to personal loans or emergency funds. The application process is speedy, uncomplicated, and direct."
-                offerUrl={`https://www.bkoffers.com/hitstreet/redirect_tp.cfm?oid=19&sid=9478&pid=3361&eid=top-${trackingState.sid}&uid=yourUID`}
+                text="This offer caters to individuals looking to protect their families future and offers a variety of options based on your financial needs. The application process is speedy, uncomplicated, and direct."
+                offerUrl={`https://www.bkoffers.com/hitstreet/redirect_tp.cfm?oid=19&sid=9840&pid=3577&eid=bottom-${trackingState.sid}&uid=yourUID`}
                 cta="ACCESS NOW"
-            /> */}
+            />
         </div>
     );
 };
