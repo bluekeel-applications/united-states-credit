2d9652c90225f5ea909fea8844bf891c150421bf
Weston Gibler <master@Westons-MacBook-Pro.local>
2024-05-15 09:43:58 -0400

    Turned off push. Remvoed console from bk pixel

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,5 @@
-if (typeof window === "undefined") {
-    importScripts('https://track-dmspush.com/scripts/ext/script/02ey4vzekz?url='+encodeURI(self.location.hostname));
-   }
-   importScripts("https://track-dmspush.com/scripts/sw/script/02ey4vzekz?url="+encodeURI(self.location.hostname));
+// Recent pause
+// if (typeof window === "undefined") {
+//     importScripts('https://track-dmspush.com/scripts/ext/script/02ey4vzekz?url='+encodeURI(self.location.hostname));
+//    }
+//    importScripts("https://track-dmspush.com/scripts/sw/script/02ey4vzekz?url="+encodeURI(self.location.hostname));
