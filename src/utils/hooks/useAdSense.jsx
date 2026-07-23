5eea17d5b6054c93aa9eaba41a30465d7adbd4d4
Weston Gibler <weston@bluekeel.com>
2026-07-23 14:10:19 -0400

    surpression of pid 3655

--- src/utils/hooks/useAdSense.jsx
+++ src/utils/hooks/useAdSense.jsx
@@ -2,26 +2,19 @@ import { useEffect } from 'react';
 
 const useAdSense = (tracking) => {
 
-	const { SID } = tracking;
+	const { SID, PID } = tracking;
     const sid = Number(SID);
+    const pid = Number(PID);
     // const setLive = useRef(false);
     const badSids = [
         10007
     ];
-
-    // const setLiveTimeout = () => {
-    //     setTimeout(() => {
-    //         setLive.current = true;
-    //     }, 4000);
-    // };
-
-    // useEffect(() => {
-    //     setLiveTimeout();
-    // }, []);
+    const badPids = [
+        3655
+    ];
 
 	useEffect(() => {
-   
-        if(!!sid && sid !== 'undefined' && !badSids.includes(sid)) {
+        if(!!sid && sid !== 'undefined' && !badSids.includes(sid) && !badPids.includes(pid)) {
             // Check if script already exists to avoid duplicates
             const existingScript = document.querySelector('script[src*="pagead2.googlesyndication.com"]');
             
@@ -34,36 +27,8 @@ const useAdSense = (tracking) => {
                 document.head.appendChild(script);
             }
         };
-            // Check if script already exists to avoid duplicates
-        // const existingScript = document.querySelector('script[src*="pagead2.googlesyndication.com"]');
-        
-        // if (!existingScript) {
-        //     const script = document.createElement('script');
-        //     script.async = true;
-        //     script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5105418052368941';
-        //     script.crossOrigin = 'anonymous';
-            
-        //     document.head.appendChild(script);
-        // }
-        
 		// eslint-disable-next-line
 	}, []);
-	// useEffect(() => {
-    //     if(!!sid && sid !== 'undefined' && !badSids.includes(sid)) {
-    //         // Check if script already exists to avoid duplicates
-    //         const existingScript = document.querySelector('script[src*="pagead2.googlesyndication.com"]');
-            
-    //         if (!existingScript) {
-    //             const script = document.createElement('script');
-    //             script.async = true;
-    //             script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5105418052368941';
-    //             script.crossOrigin = 'anonymous';
-                
-    //             document.head.appendChild(script);
-    //         }
-    //     };
-	// 	// eslint-disable-next-line
-	// }, [sid]);
 
 	return;
 };
